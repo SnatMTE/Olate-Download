3b535fc2d92fa5ee6e110b8b638090ae7be8b12b
Snat <me@snat.co.uk>
2025-12-16 15:08:09 +0000

    Add upgrade scripts for Olate Download 3.3.0
    - Introduced new upgrade script for upgrading from version 3.1.0 to 3.2.0, including new database fields and permissions.
    - Added upgrade script for transitioning from 3.2.1 to 3.3.0-Beta, implementing new features and database changes.
    - Created upgrade script for moving from 3.3.0-Alpha to 3.3.0-Beta, adding new permissions and modifying existing database structures.
    - Implemented a new installation script for version 3.5.0, ensuring proper database setup and configuration.
    - Enhanced error handling and user feedback during the upgrade process.

---
+++ upload/setup/install/index.php
@@ -0,0 +1,160 @@
+<?php
+/**********************************
+* Olate Download 3.3.0
+* https://github.com/SnatMTE/Olate-Download/
+**********************************
+* Copyright Olate Ltd 2005
+*
+* @author $Author: dsalisbury $ (Olate Ltd)
+* @version $Revision: 125 $
+* @package od
+*
+* Original Author: Olate Download
+* Updated by: Snat
+* Last-Edited: 2025-12-16
+*/
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>Olate Download 3.5.0 - Installation</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
+<link href="../setup.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+<table cellspacing="0" cellpadding="0" id="main">
+<tr id="logo"><td colspan="2"><img src="../images/logo.gif" width="190" height="68" alt="Logo"/></td></tr>
+<tr id="top">
+	<td id="left">Install Progress</td>
+	<td id="right">Olate Download 3 Setup</td>
+</tr>
+
+<tr>
+	<td id="progress">
+		<ul>
+		<li class="first"><span>Start</span></li>
+			<ul class="second">
+				<li class="done">Action Selection</li>
+				<li class="error">Server Requirements</li>
+				<li>License Agreement</li>
+			</ul>
+		<li class="first"><span>Configuration</span></li>
+			<ul class="second">
+				<li>Database Settings</li>
+				<li>General Settings</li>
+			</ul>
+		<li class="first"><span>Post-Install</span></li>
+			<ul class="second">
+				<li>Remove Installer Files</li>
+				<li>Login</li>
+			</ul>
+		<li class="complete">Complete!</li>
+		</ul>	
+	</td>
+	<td id="content">
+		<h1>Server Requirements </h1>
+		<p id="intro">Olate Download has a number of minimum requirements for installation. These requirements are checked below, and once satisfied you will be able to continue with the installation process.</p>
+		<br />
+				
+		<div id="options">
+			<h3>Server Requirements</h3>			
+			<p>The following are required before you can install Olate Download 3.5.0:</p>
+			<?php
+			// PHP Version Checking
+			if (phpversion() >= '4.3.0')
+			{
+			?>
+				<p>&#8226; PHP 4.3.0+ <span style="color:#009900">Test Passed - You are running PHP <?php echo phpversion(); ?></span></p>
+			<?php
+			}
+			else
+			{
+				$requirements = false;
+			?>
+				<p>&#8226; PHP 4.3.0+ <span style="color:#CC0000">Test Failed - You are running PHP <?php echo phpversion(); ?></span></p>
+			<?php
+			}
+			
+// Database support checking (accept ext/mysql OR PDO with mysql/sqlite drivers)
+		$has_mysql_ext = extension_loaded('mysql');
+		$has_pdo = extension_loaded('pdo');
+		$pdo_drivers = array();
+		$has_pdo_mysql = false;
+		$has_pdo_sqlite = false;
+		if ($has_pdo && class_exists('PDO')) {
+			$pdo_drivers = PDO::getAvailableDrivers();
+			$has_pdo_mysql = in_array('mysql', $pdo_drivers);
+			$has_pdo_sqlite = in_array('sqlite', $pdo_drivers);
+		}
+		if ($has_mysql_ext)
+		{
+		?>
+			<p>&#8226; MySQL <span style="color:#009900">Test Passed - MySQL extension is available.</span></p>
+		<?php
+		}
+		elseif ($has_pdo && ($has_pdo_mysql || $has_pdo_sqlite))
+		{
+			$drivers_list = htmlspecialchars(implode(', ', $pdo_drivers));
+		?>
+			<p>&#8226; PDO <span style="color:#009900">Test Passed - PDO available (drivers: <?php echo $drivers_list; ?>). Remote DB servers are allowed.</span></p>
+		<?php
+		}
+		else
+		{
+			$requirements = false;
+		?>
+			<p>&#8226; Database support <span style="color:#CC0000">Test Failed - MySQL or PDO (with pdo_mysql/pdo_sqlite) not available.</span></p>
+			<?php
+			}
+			
+			// Check config.php is writable
+			if (is_writable('../../includes/config.php') && file_exists('../../includes/config.php'))
+			{
+			?>
+				<p>&#8226; includes/config.php server writable <span style="color:#009900">Test Passed</span></p>
+			<?php
+			}
+			else
+			{
+				$requirements = false;
+			?>
+				<p>&#8226; includes/config.php server writable <span style="color:#CC0000">Test Failed - You need to chmod this file to 777 and/or change file permissions to allow server writing.</span></p>
+			<?php
+			}
+			
+			// Check config.php is writable
+			if (is_writable('../../uploads'))
+			{
+			?>
+				<p>&#8226; uploads/ server writable <span style="color:#009900">Test Passed</span></p>
+			<?php
+			}
+			else
+			{
+				$requirements = false;
+			?>
+				<p>&#8226; uploads/ server writable <span style="color:#CC0000">Test Failed - You need to chmod this directory to 777 and/or change file permissions to allow server writing for file uploads.</span></p>
+			<?php
+			} 
+			
+			if (!isset($requirements))
+			{
+			?>
+				<div class="begin"><a href="1.php">Continue</a></div><br />
+			<?php
+			}
+			else
+			{
+			?>
+					<p>You need to fix the above problem(s) before you can continue with the installation. If you need assistance, please <a href="https://github.com/SnatMTE/Olate-Download/issues" target="_blank">get help</a>.</p>
+			<?php
+			}
+			?>				
+		</div>
+	</td>
+</tr>
+</table>
+
+</body>
+</html>
